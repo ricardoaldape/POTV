b0654ea93187dee9eb176da0321e0cb8b4990df1
Ricardo Aldape <riarrual@gmail.com>
2026-09-23 10:08:52 -0600

    Add default manifests for VOD, anime, sports and live TV

--- lib/config/default_manifests.dart
+++ lib/config/default_manifests.dart
@@ -2,15 +2,22 @@ class DefaultManifests {
   const DefaultManifests._();
 
   static const List<String> urls = [
-    // 🍥 Anime (Latino/Subtitulado)
-    'https://anime-kitsu.strem.fun/manifest.json',
-    'https://pigamer37.alwaysdata.net/manifest.json',
-
-    // ⚽ Deportes en Vivo
-    'https://nuviosports.xyz/manifest.json',
-
-    // 🎬 VOD General (Latino/Subtitulado)
+    // VOD (Latino/Subtitulado)
     'https://addontb.cinehack.net/manifest.json',
     'https://torrentio.strem.fun/manifest.json',
+
+    // Anime (Latino/Subtitulado)
+    'https://pigamer37.alwaysdata.net/manifest.json',
+    'https://anime-kitsu.strem.fun/manifest.json',
+    'https://aniliststream.edmit.in/manifest.json',
+
+    // Deportes en Vivo
+    'https://nuviosports.xyz/manifest.json',
+    'https://848b3516657c-argentinatv.baby-beamup.club/manifest.json',
+    'https://raw.githubusercontent.com/yowmamasita/usa-tv-next/main/manifest.json',
+
+    // TV en Vivo
+    'https://rtve-stremio-addon.vercel.app/manifest.json',
+    'https://ftv-stremio.surge.sh/manifest.json',
   ];
 }
