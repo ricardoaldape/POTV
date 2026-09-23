a3eabdf853de8c2ae298190c3a4942fa5e15bec2
ricardoaldape <80651393+ricardoaldape@users.noreply.github.com>
2026-09-23 06:59:24 -0600

    Update default_manifests.dart

--- lib/config/default_manifests.dart
+++ lib/config/default_manifests.dart
@@ -2,6 +2,15 @@ class DefaultManifests {
   const DefaultManifests._();
 
   static const List<String> urls = [
-    // El usuario añadirá aquí sus URLs.
+    // 🍥 Anime (Latino/Subtitulado)
+    'https://anime-kitsu.strem.fun/manifest.json',
+    'https://pigamer37.alwaysdata.net/manifest.json',
+
+    // ⚽ Deportes en Vivo
+    'https://nuviosports.xyz/manifest.json',
+
+    // 🎬 VOD General (Latino/Subtitulado)
+    'https://addontb.cinehack.net/manifest.json',
+    'https://torrentio.strem.fun/manifest.json',
   ];
 }
